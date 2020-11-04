57648516e504f3bd6f26232489cbc3e51329363e
Cedric Stephani <cedric.wanaka@gmail.com>
2020-11-04 18:40:20 +1300

    working on components

--- classic-games/src/screens/Home.js
+++ classic-games/src/screens/Home.js
@@ -4,6 +4,10 @@ function Home(props) {
   return (
     <div id="main-styling">
       <h1>This will be the homepage of the app :)</h1>
+      <p>
+        the first game I will be implementing is{" "}
+        <a href="./games/tetris">tetris</a>
+      </p>
     </div>
   );
 }
