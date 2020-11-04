f806f679ca73abfeda7c84894373538f6ab47e4e
Cedric Stephani <cedric.wanaka@gmail.com>
2020-11-04 20:18:29 +1300

    added I and J shapes of the tetris game

---
+++ classic-games/src/tetronomis.js
@@ -0,0 +1,22 @@
+//This class holds the different shapes allowed inthe Tetris minigame.
+
+export const TETRONOMIS = {
+  0: { shape: [[0]], color: "0,0,0" },
+  I: {
+    shape: [
+      [0, "I", 0, 0],
+      [0, "I", 0, 0],
+      [0, "I", 0, 0],
+      [0, "I", 0, 0],
+    ],
+    color: "80,227,230",
+  },
+  J: {
+    shape: [
+      [0, "J", 0],
+      [0, "J", 0],
+      ["J", "J", 0],
+    ],
+    color: "0,0,0",
+  },
+};
