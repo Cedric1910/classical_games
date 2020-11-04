2574c59d2b7afb35c69b1afe2058d5ae94b1dea5
Cedric Stephani <cedric.wanaka@gmail.com>
2020-11-04 17:42:52 +1300

    added screens folder and homepage and tetris page

---
+++ classic-games/src/screens/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Home(props) {
+  return (
+    <div id="main-styling">
+      <h1>This will be the homepage of the app :)</h1>
+    </div>
+  );
+}
+export default Home;
---
+++ classic-games/src/screens/Tetris.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Tetris(props) {
+  return (
+    <div id="main-styling">
+      <h1>This will be the Tetris game page :)</h1>
+    </div>
+  );
+}
+
+export default Tetris;
