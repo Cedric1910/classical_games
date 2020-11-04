584369efcca658f43d5df51d121f7f84787b7f81
Cedric Stephani <cedric.wanaka@gmail.com>
2020-11-04 21:43:41 +1300

    added home bg image

--- classic-games/src/screens/Home.js
+++ classic-games/src/screens/Home.js
@@ -3,12 +3,14 @@ import "../index.css";
 
 function Home(props) {
   return (
-    <div id="main-styling">
-      <h1>This will be the homepage of the app :)</h1>
-      <p>
-        the first game I will be implementing is{" "}
-        <a href="./games/tetris">tetris</a>
-      </p>
+    <div id="home-bg">
+      <div id="main-styling">
+        <h1>This will be the homepage of the app :)</h1>
+        <p>
+          the first game I will be implementing is{" "}
+          <a href="./games/tetris">tetris</a>
+        </p>
+      </div>
     </div>
   );
 }
--- classic-games/src/screens/Tetris.js
+++ classic-games/src/screens/Tetris.js
@@ -8,17 +8,19 @@ import "../index.css";
 
 function Tetris(props) {
   return (
-    <div id="main-styling">
-      <div>
-        <Stage stage={createStage()}></Stage>
-        <aside>
-          <div id="tetris-displays">
-            <Display text="Score:"></Display>
-            <Display text="Rows completed:"></Display>
-            <Display text="Current Level:"></Display>
-          </div>
-          <Start_button></Start_button>
-        </aside>
+    <div id="tetris-bg">
+      <div id="main-styling">
+        <div>
+          <Stage stage={createStage()}></Stage>
+          <aside>
+            <div id="tetris-displays">
+              <Display text="Score:"></Display>
+              <Display text="Rows completed:"></Display>
+              <Display text="Current Level:"></Display>
+            </div>
+            <Start_button></Start_button>
+          </aside>
+        </div>
       </div>
     </div>
   );
