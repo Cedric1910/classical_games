dd9f658e8ee9c0ecf2bf4ffc41c2c4f11e5c7cd0
Cedric Stephani <cedric.wanaka@gmail.com>
2020-11-04 19:27:55 +1300

    fixed error in GameHelpers causing app to stop running

--- classic-games/src/gameHelpers.js
+++ classic-games/src/gameHelpers.js
@@ -4,7 +4,6 @@ export const STAGE_HEIGHT = 20;
 
 //creates the stage which will be used for the tetris stage
 export const createStage = () =>
-  Array.form(
-    Array(STAGE_HEIGHT),
-    () => new Array(STAGE_WIDTH.fill([0, "clear"]))
+  Array.from(Array(STAGE_HEIGHT), () =>
+    new Array(STAGE_WIDTH).fill([0, "clear"])
   );
