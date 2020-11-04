b7a5aff8baca47017eae21275408dff54bda3ddb
Cedric Stephani <cedric.wanaka@gmail.com>
2020-11-05 11:27:28 +1300

    added styled start button to tetris

--- classic-games/src/components/Start_button.js
+++ classic-games/src/components/Start_button.js
@@ -1,6 +1,9 @@
 import React from "react";
 import Stage from "./Stage";
+import { StartButton } from "./styles/StyledStartButton";
 
-const Start_button = ({ callback }) => <div>Start Game</div>;
+const Start_button = ({ callback }) => (
+  <StartButton onClick={callback}> Start game </StartButton>
+);
 
 export default Start_button;
