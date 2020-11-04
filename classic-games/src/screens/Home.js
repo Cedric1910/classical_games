b3aef7aa40ab1e946cc7e28da9abe8c3a5effbe9
Cedric Stephani <cedric.wanaka@gmail.com>
2020-11-05 12:37:37 +1300

    updated homepage and css styling with minor improvements to make it readable

--- classic-games/src/screens/Home.js
+++ classic-games/src/screens/Home.js
@@ -7,8 +7,10 @@ function Home(props) {
       <div className="main-styling">
         <h1>This will be the homepage of the app :)</h1>
         <p>
-          the first game I will be implementing is{" "}
-          <a href="./games/tetris">tetris</a>
+          the first game I will be implementing is my visualisation of the game{" "}
+          <a href="./games/tetris">Tetris</a>. <br></br>The homepage is on my
+          to-do list of giving a full visual upgrade with a more responsive and
+          clean looking design. Check back for updates.
         </p>
       </div>
     </div>
