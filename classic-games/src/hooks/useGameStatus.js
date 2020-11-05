22e808c22b086b149ab1e999cadc2eba0e4b4db2
Cedric Stephani <cedric.wanaka@gmail.com>
2020-11-05 22:22:02 +1300

    added GameStatus file to keep track of current game progression

---
+++ classic-games/src/hooks/useGameStatus.js
@@ -0,0 +1,7 @@
+import { useState, useEffect, useCallback } from "react";
+
+export const gameStatus = (rowsCleared) => {
+  const [rows, setRows] = useState(0);
+  const [score, setScore] = useState(0);
+  const [level, setLevel] = useState(0);
+};
