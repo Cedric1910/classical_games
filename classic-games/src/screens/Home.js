e0c36e1b95732a106079f99b5702b3a5aa7e7784
Cedric Stephani <cedric.wanaka@gmail.com>
2020-11-05 23:37:18 +1300

    added a small bio to the homepage with Tetris being almost complete

--- classic-games/src/screens/Home.js
+++ classic-games/src/screens/Home.js
@@ -5,12 +5,22 @@ function Home(props) {
   return (
     <div className="home-bg">
       <div className="main-styling">
-        <h1>This will be the homepage of the app :)</h1>
+        <h1>Cedric's old school arcade.</h1>
+        <h3>
+          This is just a fun solo project im doing in my spare time with some of
+          my favourite simple games<br></br>
+          from back in the day. If you have any idea which one I should
+          implement next let me know!
+        </h3>
         <p>
-          the first game I will be implementing is my visualisation of the game{" "}
-          <a href="./games/tetris">Tetris</a>. <br></br>The homepage is on my
-          to-do list of giving a full visual upgrade with a more responsive and
-          clean looking design. Check back for updates.
+          Current Games: <br></br>
+          <a href="./games/tetris">Tetris</a>. <br></br>
+          <br></br>
+          Yes this homepage looks very unfinished. As you can see down the
+          bottom right hand corner we are in the early versions of this<br></br>
+          application. In version 2 I will be re-designing the Homepage to give
+          it a more modern and responsive look, but first I wanted to get at
+          least 1 game working.
         </p>
       </div>
     </div>
