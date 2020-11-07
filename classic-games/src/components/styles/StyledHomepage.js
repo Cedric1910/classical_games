130244f685fb56f4d9f367b6373552045c7f1703
Cedric Stephani <cedric.wanaka@gmail.com>
2020-11-07 15:49:44 +1300

    got rid of old homepage text

--- classic-games/src/components/styles/StyledHomepage.js
+++ classic-games/src/components/styles/StyledHomepage.js
@@ -1,12 +1,17 @@
 import styled from "styled-components";
 
-export const StyledMainDisplay = styled.div`
+export const StyledHomepageLayout = styled.div`
   width: 100%;
+  height: 100%;
+  min-height: 35rem;
+  border: solid 1px red;
   color: white;
   text-align: center;
-
   a:link,
   a:visited {
     color: red;
   }
+  display: flex;
 `;
+
+export const StyledHomepageMenu = styled.div``;
--- classic-games/src/screens/Home.js
+++ classic-games/src/screens/Home.js
@@ -1,29 +1,11 @@
 import React from "react";
 import "../index.css";
-import { StyledMainDisplay } from "../components/styles/StyledHomepage";
+import { StyledHomepageLayout } from "../components/styles/StyledHomepage";
 
 function Home(props) {
   return (
     <div className="home-bg">
-      <StyledMainDisplay>
-        <h1>Cedric's old school arcade.</h1>
-        <h3>
-          This is just a fun solo project im doing in my spare time with some of
-          my favourite simple games<br></br>
-          from back in the day. If you have any idea which one I should
-          implement next let me know!
-        </h3>
-        <p>
-          Current Games: <br></br>
-          <a href="./games/tetris">Tetris</a>. <br></br>
-          <br></br>
-          Yes this homepage looks very unfinished. As you can see down the
-          bottom right hand corner we are in the early versions of this<br></br>
-          application. In version 2 I will be re-designing the Homepage to give
-          it a more modern and responsive look, but first I wanted to get at
-          least 1 game working.
-        </p>
-      </StyledMainDisplay>
+      <StyledHomepageLayout></StyledHomepageLayout>
     </div>
   );
 }
