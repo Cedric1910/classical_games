c12e4045385fcfbbaa65a25c5eda79465a745654
Cedric Stephani <cedric.wanaka@gmail.com>
2020-11-07 21:03:40 +1300

    sorted out spacing and font styling depending on pages

--- classic-games/src/screens/Tetris.js
+++ classic-games/src/screens/Tetris.js
@@ -114,7 +114,7 @@ function Tetris(props) {
               ) : (
                 <div id="tetris-displays">
                   <Display text={`Score: ${score}`}></Display>
-                  <Display text={`Rows Completed: ${rows}`}></Display>
+                  <Display text={`Rows: ${rows}`}></Display>
                   <Display text={`Level: ${level + 1}`}></Display>
                 </div>
               )}
