615f88febb1c8e365f8a5405c6993b8f8ab5316d
Cedric Stephani <cedric.wanaka@gmail.com>
2020-12-23 14:26:35 +1300

    added latest changes for new release

--- classic-games/src/hooks/useGameStatus.js
+++ classic-games/src/hooks/useGameStatus.js
@@ -4,7 +4,6 @@ export const useGameStatus = (rowsCleared) => {
   const [score, setScore] = useState(0);
   const [rows, setRows] = useState(0);
   const [level, setLevel] = useState(0);
-
   const linePoints = [40, 100, 300, 1200];
 
   const calcScore = useCallback(() => {
